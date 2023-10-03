fbfb9b8fdd045074df02bd0e166793a8bb5e6559
Baptiste Roland <baptiste.roland@edu.hefr.ch>
2023-10-03 09:44:50 +0200

    Update uuid command
    List only current tasks

--- commands/calendar/uuid.js
+++ commands/calendar/uuid.js
@@ -13,8 +13,11 @@ module.exports = {
     if (entries.length == 0) return interaction.reply({ content: "You have nothing to do", ephemeral: true });
 
     let out = "";
-    for (const [key, value] of entries) {      
-      out += "**" + key + "** | " + value.end + " - " + value.lesson + ": " + value.title + "\n";
+    for (const [key, value] of entries) {    
+      if (Date.parse(value.end) > Date.now() - 3 * 24 * 60 * 60 * 1000)
+      {  
+        out += "**" + key + "** | " + value.end + " - " + value.lesson + ": " + value.title + "\n";
+      } 
     }
 
     return interaction.reply({ content: out, ephemeral: false });
